75e59f1ebeaf7f276404326b1692d0ee2dabbf28
Rafael <rflernesto@gmail.com>
2021-06-25 00:36:48 -0400

    feature: add dev vpc to main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -2,6 +2,19 @@ provider "aws" {
     region = "sa-east-1"
 }
 
+
+resource "aws_vpc" "dev-vpc" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+      Name = "dev-vpc"
+    }
+}
+
+resource "aws_subnet" "public-dev-1" {
+  vpc_id = aws_vpc.dev-vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
 resource "aws_instance" "dev-1" {
     ami = "ami-0c9c4f3290bbba7b4 "
     instance_type = "t2.micro"
